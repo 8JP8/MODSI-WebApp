f532ccecc8110a4e59295e91b5d26793827765db
João Oliveira <1240369@isep.ipp.pt>
2025-05-21 10:39:51 +0100

    try to fix

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,6 @@ const App = () => (
               <Route path="/configurator" element={<VRDashboard />} />
               <Route path="/login" element={<Login />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
         </AuthProvider>
