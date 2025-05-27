1f2b6d4ca9383acc50e46c5abc902b3e93fcb4a3
JͮⳎ <90798978+8JP8@users.noreply.github.com>
2025-05-27 02:01:24 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -36,6 +35,7 @@ const App = () => (
                 </RequireAuth>
               } />
               <Route path="/login" element={<Login />} />
+              <Route path="/passwordreset" element={<PasswordReset />} />
               <Route path="/passwordreset/:code" element={<PasswordReset />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
